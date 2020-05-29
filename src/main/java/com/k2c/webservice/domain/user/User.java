a155874acd3b98b856a529ec9e4159e72a12ad27
최기환 <>
2020-05-29 13:16:07 +0900

    수정사항 반영

---
+++ src/main/java/com/k2c/webservice/domain/user/User.java
@@ -0,0 +1,13 @@
+package com.k2c.webservice.domain.user;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class User {
+}
